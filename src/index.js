4a65ffe0050cb09118f316d768643db572324a7a
luancma <luancmadev@gmail.com>
2022-09-10 14:12:21 -0300

    add pre-commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-const liveServer = require("live-server");
+const liveServer = require('live-server')
 
 const params = {
-  ignore: "scss,my/templates", // comma-separated string for paths to ignore
-  root: "./src/public", // Set root directory that's being served. Defaults to cwd.
+  ignore: 'scss,my/templates', // comma-separated string for paths to ignore
+  root: './src/public', // Set root directory that's being served. Defaults to cwd.
   wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
   logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
   middleware: [
     function (req, res, next) {
-      next();
-    }
-  ]
-};
+      next()
+    },
+  ],
+}
 
-liveServer.start(params);
+liveServer.start(params)
